a9063b71086973b91aec448349b7b844209bf2ab
Pratik Kenaudekar <scorp86@gmail.com>
2020-10-26 12:57:51 +0530

    Initial Commit

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import youtube from '../apis/youtube';
+const keys = require('../config/dev');
+
+const KEY = keys.goolgeKey;
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
+      },
+    });
+
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;
